a961e9761f2c1f7bbcad002adf0950bb874dd18b
Klava147 <123240109@student.upnyk.ac.id>
2026-05-07 08:45:52 +0700

    add mvc architecture

---
+++ appInventarisToko/src/controller/Makanan.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controller;
+
+import model.Barang;
+
+
+/**
+ *
+ * @author umair
+ */
+
+public class Makanan extends Barang{
+    public Makanan() {
+        
+    }
+}
